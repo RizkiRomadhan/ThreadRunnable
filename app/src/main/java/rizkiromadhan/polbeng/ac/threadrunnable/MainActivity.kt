a20870d80c5ea8781af191e38cadf539e42b6d21
RPL-32 <rizkiromadhanbks298@gmail.com>
2022-04-23 11:34:02 +0700

    Thread Runnable

---
+++ app/src/main/java/rizkiromadhan/polbeng/ac/threadrunnable/MainActivity.kt
@@ -0,0 +1,25 @@
+package rizkiromadhan.polbeng.ac.threadrunnable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Thread(Runnable {
+                killSomeTime()
+            }).start()
+        }
+    }
+    private fun killSomeTime() {
+        for (i in 1..20) {
+            runOnUiThread(Runnable{
+                textView.text = i.toString()
+            })
+            println("i:$i")
+            Thread.sleep(2000)
+        }
+    }
+}
